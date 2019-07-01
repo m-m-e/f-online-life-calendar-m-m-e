ecdbb2af7c391a478b2481d9ff85b1d80f5bdd4d
m-m-e <megmyfedwards@gmail.com>
2019-07-01 10:17:30 +0200

    Add docs folder

---
+++ docs/precache-manifest.27310d923294d161a8ccf397f39642ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75f518e43ac55bc7cd8d55b2c93286c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd1f31ed6b21af234926",
+    "url": "/static/css/main.9e1bfd2e.chunk.css"
+  },
+  {
+    "revision": "7e8cfe442633095afcfd",
+    "url": "/static/js/2.e8d0f329.chunk.js"
+  },
+  {
+    "revision": "cd1f31ed6b21af234926",
+    "url": "/static/js/main.f14dc794.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
