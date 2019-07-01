fb1b42521e2cc9ce0d519622ef2f812aa4a94d70
m-m-e <megmyfedwards@gmail.com>
2019-07-01 11:48:11 +0200

    Add modal and update docs

---
+++ docs/precache-manifest.650f130497801cd65966b9032f4e3240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392378ad6195d8f3fcbf5645eb33a1b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f5ad2e5859a7dc587df",
+    "url": "./static/css/main.d1e71988.chunk.css"
+  },
+  {
+    "revision": "7e8cfe442633095afcfd",
+    "url": "./static/js/2.e8d0f329.chunk.js"
+  },
+  {
+    "revision": "9f5ad2e5859a7dc587df",
+    "url": "./static/js/main.1f6f4c78.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27310d923294d161a8ccf397f39642ca.js"
+  "./precache-manifest.650f130497801cd65966b9032f4e3240.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
